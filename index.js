62aae78d349ed95c746650db5d7287379ba3dd8f
zdobx <zdobson@protonmail.com>
2023-05-15 21:05:10 +1000

    Added dropdown menu functionality

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Skeleton comments for dynamic interface odin project 
+
+// function navBar {
+//     let navItems = document.getElementsByClassName("nav-visible");
+//     let exploreHeader = document.getElementById("explore");
+
+//     if exploreHeader.addEventListener("hover");
+//         navItems.style.display = 'none';
+
+// }
+
+
+
+
+//Example code from previous dynamic sidebar 
+let btn = document.querySelector(`.exploreBtn`);
+let dropdown = document.querySelector(`.dropdown`);
+
+
+btn.onclick = function () {
+  dropdown.classList.toggle("active");
+};
+
+
